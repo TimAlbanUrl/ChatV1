e518faf13fd5eaddd60af4b682528499d6c23225
TimAlbanUrl <mballu100@gmail.com>
2024-02-12 16:23:37 +0100

    build: layout

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 <template>
   <header>
   </header>
-  <div class="bg-gray-900 text-white" style="width: 100%; height: 100%;">
+  <div class="bg-gray-900 text-white flex flex-row" style="width: 100%; height: 100%;">
     <RouterView />
   </div>
 </template>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -7,17 +7,17 @@ const messageText = ref('');
 const messageList = ref([]);
 
 const addMessage = () => {
-    messageList.value.push({
+    messageList.value.unshift({
         id: Math.random().toString(32).slice(2),
         text: messageText.value,
         date: new Date(),
         user: {
             username: "User",
             avatarUrl: "https://sm.ign.com/ign_fr/cover/a/avatar-gen/avatar-generations_bssq.jpg",
-        }
+        },
+        super: true
     });
     messageText.value = ""
-    messageText.value.focus()
 };
 
 const deleteMessage = (id) => {
@@ -28,10 +28,10 @@ const deleteMessage = (id) => {
 
 <template>
     <ul class="list-none p-4">
-        <li>
+        <li class="flex justify-center w-message-box">
             <textarea 
             @keyup.enter.exact="addMessage"
-            v-model="messageText" class="text-black rounded-md ml-3" name="message" id="message" cols="30" rows="1"></textarea>
+            v-model="messageText" class="p-2 text-white bg-gray-900 border-2 border-gray-900 border-b-gray-300 rounded-md ml-3 not-resizable-ta" name="message" id="message" cols="30" rows="1"></textarea>
             <button @click="addMessage" class="p-2 bg-blue-500 rounded-full ml-3">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
